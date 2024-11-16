b4946f757148a70afca5dd2b29beee02ed1fe6a9
Jeferson Pilligua <jefer.pilligua1509@gmail.com>
2024-11-16 15:00:58 +0000

    contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = ({ secondSix, secondFive, secondFour, secondThree, secondTwo, secondOne }) => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>SIMPLE COUNTER</h1>
+			<div className="seconds">
+				<div className="lineSix">{secondSix % 10}</div>
+				<div className="lineFive">{secondFive % 10}</div>
+				<div className="lineFour">{secondFour % 10}</div>
+				<div className="lineThree">{secondThree % 10}</div>
+				<div className="lineTwo">{secondTwo % 10}</div>
+				<div className="lineOne">{secondOne % 10}</div>
+			</div>
 		</div>
 	);
 };
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,16 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let counter = 0;
+setInterval(() => {
+    const six = Math.floor(counter / 100000);
+    const five = Math.floor(counter / 10000);
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
 
+
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home secondOne={one} secondTwo={two} secondThree={three} secondFour={four} secondFive={five} secondSix={six}/>);
+},1000);
